5e9f45f7b8151a8fbb30417a03705470db4435de
timdbie <timdebie67@gmail.com>
2024-09-16 17:56:41 +0200

    feat: Added fixed height

--- frontend/src/components/Button/Button.tsx
+++ frontend/src/components/Button/Button.tsx
@@ -4,7 +4,7 @@ import { Icon } from '@iconify/react';
 
 const Button: React.FC<ButtonProps> = ({ label, icon, backgroundColor, onClick }) => (
   <button
-    className="p-2 flex items-center text-lg border-solid border-black border-2 rounded-xl shadow"
+    className="h-12 p-2 flex items-center text-lg border-solid border-black border-2 rounded-xl shadow"
     style={{ backgroundColor }}
     onClick={onClick}
   >
