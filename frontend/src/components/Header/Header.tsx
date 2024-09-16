0dc901b7a19708121b03533453f8692ed6b6d8ca
timdbie <timdebie67@gmail.com>
2024-09-16 19:46:08 +0200

    feat: Added header nav

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,13 +1,20 @@
+import { Icon } from "@iconify/react/dist/iconify.js";
 import React from "react";
 
 const Header: React.FC = () => (
     <header className="text-black">
         <div className="h-24 flex justify-between items-center">
             <h1 className="text-3xl font-semibold">TimpieTyper</h1>
-            <nav>
-                <a href="#">C</a>
-                <a href="#">C</a>
-                <a href="#">C</a>
+            <nav className="flex">
+                <a className="mx-1" href="#">
+                    <Icon icon="mingcute:trophy-line" fontSize="1.8rem" />
+                </a>
+                <a className="mx-1" href="#">
+                    <Icon icon="mingcute:user-2-line" fontSize="1.8rem" />
+                </a>
+                <a className="mx-1" href="#">
+                    <Icon icon="mingcute:settings-5-line" fontSize="1.8rem" />
+                </a>
             </nav>
         </div>
     </header>
