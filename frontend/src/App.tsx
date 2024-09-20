7a55e1e30ff836d7c0683a285d6732986a8c7cd2
timdbie <timdebie67@gmail.com>
2024-09-20 16:09:13 +0200

    feat: Split up typer and bar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,17 @@
 import './index.css'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
-import Main from './components/Main/Main'
+import Bar from './components/Bar/Bar'
+import Typer from './components/Typer/Typer'
 
 function App() {
   return (
     <div className="container flex flex-col h-screen">
       <Header />
-      <Main />
+      <div className="flex flex-1 justify-center items-center">
+        <Bar />
+        <Typer />
+      </div>
       <Footer />
     </div>
   )
---
+++ frontend/src/components/Typer/Typer.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Typer: React.FC = () => (
+    <div></div>
+);
+
+export default Typer;
