d1077869c74faa429fa4eaad1143dd3f3711a4a9
timdbie <timdebie67@gmail.com>
2024-09-25 16:07:36 +0200

    feat: Added word list

--- src/components/Typer/Typer.tsx
+++ src/components/Typer/Typer.tsx
@@ -1,9 +1,19 @@
 import React from "react";
 
-const Typer: React.FC = () => (
-    <div className="w-full h-36 bg-blue-700">
-        <div>
+const words = Array(64).fill("hello");
 
+const Typer: React.FC = () => (
+    <div className="w-full h-36 overflow-clip">
+        <div className="w-full flex flex-wrap overflow-clip">
+            {words.map((word, index) => (
+                <div key={index} className="word">
+                    {word.split('').map((letter: string, letterIndex: number) => (
+                        <span key={letterIndex}>
+                            {letter}
+                        </span>
+                    ))}
+                </div>
+            ))}
         </div>
     </div>
 );
