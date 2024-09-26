c3a077ceb89b9f145cbb8fdc4017c12286a3d4ea
timdbie <timdebie67@gmail.com>
2024-09-26 16:43:43 +0200

    feat: Added individual letter tracking

--- src/components/Typer/Typer.tsx
+++ src/components/Typer/Typer.tsx
@@ -7,44 +7,75 @@ const Typer: React.FC = () => {
     const [inputValue, setInputValue] = useState('');
     const [activeIndex, setActiveIndex] = useState(0);
 
-    const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const value = e.target.value;
-        setInputValue(value);
+    const updateLetters = (value: string) => {
+        const activeWord = document.querySelector('.word.active');
+        if (activeWord) {
+            const letters = activeWord.querySelectorAll('span');
+            letters.forEach((letter, letterIndex: number,) => {
+                const isCorrect = value[letterIndex] === words[activeIndex][letterIndex];
 
-        if (value === words[activeIndex]) {
-            if (activeIndex < words.length - 1) {
-                setActiveIndex(activeIndex + 1);
-            }
-            setInputValue('');
+                if (letterIndex < value.length) {
+                    if (isCorrect) {
+                        letter.className = 'correct';
+                    } else {
+                        letter.className = 'incorrect';
+                    }
+                } else {
+                    letter.removeAttribute('class');
+                }
+            });
         }
     };
 
+    const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const value = e.target.value;
+        setInputValue(value);
+        updateLetters(value);
+        
+        if (value.length === words[activeIndex].length) {
+            if (activeIndex < words.length - 1) {
+                setActiveIndex(activeIndex + 1);
+            }
+
+            setInputValue('');
+        }
+    };    
+
     return (
         <div className="relative w-full h-36 overflow-clip">
-            <input 
-                className="absolute bottom-0 z-30" 
+            <input
+                className="absolute bottom-0 z-30"
                 type="text"
-                autoComplete="off" 
-                autoCapitalize="off" 
-                autoCorrect="off" 
+                autoComplete="off"
+                autoCapitalize="off"
+                autoCorrect="off"
                 spellCheck="false"
                 value={inputValue}
                 onChange={handleInput}
-            >
-            </input>
+            />
             <div className="w-full flex flex-wrap overflow-clip">
-                {words.map((word, index) => (
-                    <div key={index} className={`word ${index === activeIndex ? 'active' : ''}`}>
-                        {word.split('').map((letter: string, letterIndex: number) => (
-                            <span key={letterIndex}>
-                                {letter}
-                            </span>
-                        ))}
-                    </div>
-                ))}
+                {words.map((word, index) => {
+                    let className = 'word';
+                    if (index === activeIndex) {
+                        className += ' active';
+                    } else if (index < activeIndex) {
+                        className += ' typed';
+                    }
+
+                    return (
+                        <div key={index} className={className}>
+                            {word.split('').map((letter: string, letterIndex: number) => (
+                                <span key={letterIndex}>
+                                    {letter}
+                                </span>
+                            ))}
+                        </div>
+                    );
+                })}
             </div>
         </div>
     );
 };
+
 
 export default Typer;
