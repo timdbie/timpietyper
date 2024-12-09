2f11d42c54290ef7badf7e99ee40b101629eba9d
timdbie <timdebie67@gmail.com>
2024-12-09 17:03:39 +0100

    feat: Added network

--- src/components/Typer/Typer.tsx
+++ src/components/Typer/Typer.tsx
@@ -9,20 +9,17 @@ const Typer: React.FC = () => {
 
     useEffect(() => {
         const fetchWords = async () => {
-            // try {
-            //     const response = await fetch("http://localhost:5278/api/Word/random?count=50");
-            //     if (!response.ok) {
-            //         throw new Error("Network response was not ok");
-            //     }
-            //     const data = await response.json();
-            //     setWords(data.values);
-            //     setTypedWords(new Array(data.values.length).fill(''));
-            // } catch (error) {
-            //     console.error("Error fetching words:", error);
-            // }
-          
-            setWords([]);
-            setTypedWords([]);
+            try {
+                const response = await fetch("http://timpietyper-api/api/Word/random?count=50");
+                if (!response.ok) {
+                    throw new Error("Network response was not ok");
+                }
+                const data = await response.json();
+                setWords(data.values);
+                setTypedWords(new Array(data.values.length).fill(''));
+            } catch (error) {
+                console.error("Error fetching words:", error);
+            }
         };
 
         fetchWords();
